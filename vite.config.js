1cceaf8742f3d424afd455d23529dd5d4797d16f
Blenmus <132626093+Blenmus@users.noreply.github.com>
2023-11-07 00:02:08 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This is the default output directory for your build
+  },
+});
